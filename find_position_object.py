fcf9cc2ea7f8307a8546162ff99d5cc2ff9c4191
Johan Mazoyer <johan.mazoyer@obspm.fr>
2022-03-04 15:42:24 +0100

    I think this is finished but not convinced this is working

--- find_position_object.py
+++ find_position_object.py
@@ -5,7 +5,7 @@ Created on  March 4 2022
 code to fin the position of an object to dig the dark hole at the good position with SPHERE
 @author: J Mazoyer
 
-required atroplan
+requires atroplan
 conda install -c conda-forge astroplan
 don't forget to cite https://ui.adsabs.harvard.edu/abs/2018AJ....155..128M/abstract
 """
@@ -26,16 +26,21 @@ True_North = -1.75 #+/-0.08 deg (https://arxiv.org/pdf/1609.06681.pdf)
 # test beta-pictoris irdis dataset 2015-02-05
 target_name = "Beta Pictoris"
 timestring = '2015-02-05T03:25:02' # start time
+# timestring = '2015-02-05T06:38:21' # estimated end time for 11599s total time
 estimated_onsky_PA_of_the_planet = 212.58 # https://doi.org/10.1051/0004-6361/201834302
 
 time = Time(timestring, format='isot', scale='utc')
 target = FixedTarget.from_name(target_name)
 
 PARANGLE_deg = vlt.parallactic_angle(time, target).to_value(u.deg)
-print(PARANGLE_deg)
 
 ### zeformula given by Gael
 ### PA_onsky = PA_detector + PARANGLE_deg + True_North + PUPOFFSET 
 
 PA_detector = estimated_onsky_PA_of_the_planet - PARANGLE_deg - True_North - PUPOFFSET 
-print(PA_detector)
+
+print("target: ",target_name)
+print("planet estimated Postion angle is:", estimated_onsky_PA_of_the_planet )
+print("observation time: ",timestring)
+print("parralactic angle: ", round(PARANGLE_deg,2))
+print("PA on detector angle: ", round(PA_detector,2))
