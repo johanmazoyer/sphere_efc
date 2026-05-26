017ed80884f5a210a33271c2a8b51d34d4ddfa0c
Galicher Raphael <raphael.galicher@obspm.fr>
2026-05-26 22:38:23 +0200

    replace "" by " "  in Acquire_probe

--- Acquire_Probe.sh
+++ Acquire_Probe.sh
@@ -61,7 +61,7 @@ N=300
 	do
 		echo " * loading ref slopes: ${FILE}"
 		rsh wsrsgw cdmsLoad -f ${FILE} -r VisAcq.DET1.REFSLP 
-		rsh wsrsgw "msgSend \"\" CommandGateway EXEC \"VisAcq.update ALL\""
+		rsh wsrsgw "msgSend \"\" CommandGateway EXEC \"VisAcq.update ALL\" "
 
 		echo "Waiting 3s for the slopes to be loaded"
 		/bin/sleep 3
@@ -69,9 +69,9 @@ N=300
 
 		echo "Acquire Probe"
 		echo ' * acquiring image'
-		ssh wsre "msgSend -n wsre sroControl SETUP \"-expoId 0 -file SPHERE_irdis_tec_exp.ref -function OCS1.DET1.READ.CURNAME Nondest  OCS1.DET1.SEQ1.DIT ${DIT_probe} OCS1.DET1.NDIT ${NDIT_probe} DPR.CATG TEST DPR.TYPE OBJECT DPR.TECH IMAGE OCS1.OCS.DET1.IMGNAME ${EXP_NAME}iter${nbiter}_Probe_000${which_probe}_ OCS1.DET1.FRAM1.STORE F OCS1.DET1.FRAM2.STORE T OCS1.DET1.ACQ1.QUEUE 0 OCS.DET1.IMGNAME SPHERE_IRDIS_OBS OCS1.DET1.SEQ1.WIN.STRX ${SX} OCS1.DET1.SEQ1.WIN.STRY ${SY} OCS1.DET1.SEQ1.WIN.NX 2048 OCS1.DET1.SEQ1.WIN.NY ${N}\""
-		ssh wsre "msgSend -n wsre sroControl START \"-detId IRDIS\""
-		ssh wsre "msgSend -n wsre sroControl WAIT \"-detId IRDIS\""
+		ssh wsre "msgSend -n wsre sroControl SETUP \"-expoId 0 -file SPHERE_irdis_tec_exp.ref -function OCS1.DET1.READ.CURNAME Nondest  OCS1.DET1.SEQ1.DIT ${DIT_probe} OCS1.DET1.NDIT ${NDIT_probe} DPR.CATG TEST DPR.TYPE OBJECT DPR.TECH IMAGE OCS1.OCS.DET1.IMGNAME ${EXP_NAME}iter${nbiter}_Probe_000${which_probe}_ OCS1.DET1.FRAM1.STORE F OCS1.DET1.FRAM2.STORE T OCS1.DET1.ACQ1.QUEUE 0 OCS.DET1.IMGNAME SPHERE_IRDIS_OBS OCS1.DET1.SEQ1.WIN.STRX ${SX} OCS1.DET1.SEQ1.WIN.STRY ${SY} OCS1.DET1.SEQ1.WIN.NX 2048 OCS1.DET1.SEQ1.WIN.NY ${N}\" "
+		ssh wsre "msgSend -n wsre sroControl START \"-detId IRDIS\" "
+		ssh wsre "msgSend -n wsre sroControl WAIT \"-detId IRDIS\" "
 	done
 
 
